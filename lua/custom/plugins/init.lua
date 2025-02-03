e48f3f4ee8a4598a4dc2a68c773515f209920c93
Sheila Mbadi <mbadisheila@gmail.com>
2025-02-03 15:48:33 +0300

    Add own config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+}
